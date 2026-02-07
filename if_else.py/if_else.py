edf459772d5c4b202c8b224f5e15d3c9389d45e7
boshaprogrammist228 <bosha.pro2020@gmail.com>
2026-02-07 17:50:28 +0500

    Add if-else statements for age and conditions

---
+++ if_else.py/if_else.py
@@ -0,0 +1,21 @@
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+
+
+a = 33
+b = 200
+
+if b > a:
+  pass
