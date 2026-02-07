f3f255e41d6c7cef9dd5361d86103b0b03aca0b9
boshaprogrammist228 <bosha.pro2020@gmail.com>
2026-02-07 17:44:21 +0500

    Create set.py

---
+++ sets.py/set.py
@@ -0,0 +1,15 @@
+set = {1, 2, 2, 3, 5, 5, 5}
+print(set)#will show only 1 2 3 5 because set does not allow duplicates
+
+#Set items are unchangeable, but you can remove items and add new items.
+
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.add("orange")
+
+print(thisset)
+
+set.update(thisset) ##JOINING TWO SETS
+
+
