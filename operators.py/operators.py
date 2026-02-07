a96a8fc7b2528cba1fabf505fda3f250a0943124
boshaprogrammist228 <bosha.pro2020@gmail.com>
2026-02-07 17:34:43 +0500

    Implement basic operations and comparisons
    Add basic arithmetic and conditional operations in operators.py

---
+++ operators.py/operators.py
@@ -0,0 +1,18 @@
+print(10+15)
+
+a = 5
+for i in range(5):
+  a += 1
+print(a)
+
+
+if 5 != 3:
+  print("5 is not equal to 3")
+
+
+q = 1
+w = 3
+e = 2
+if q < w and q < e:
+  print("q is the smallest number")
+
